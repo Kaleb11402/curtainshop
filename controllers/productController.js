6e5fbf613b27ad74943c7c079862d1edda25db03
Kaleb11402 <kalupp628@gmail.com>
2025-01-11 18:53:05 +0300

    Added get all categories functionality

--- controllers/productController.js
+++ controllers/productController.js
@@ -51,3 +51,20 @@ exports.createCategory = async (req, res) => {
     }
   });
 };
+
+exports.getAllCategories = async (req, res) => {
+  try {
+    const categories = await Category.findAll();
+    res.status(200).json({
+      success: true,
+      data: categories,
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({
+      success: false,
+      message: 'Failed to retrieve categories',
+      error: error.message,
+    });
+  }
+};
--- routes/products/productRoutes.js
+++ routes/products/productRoutes.js
@@ -1,10 +1,10 @@
-// routes/userRoute.js
+// routes/categoryRoute.js
 
 const express = require('express');
 const router = express.Router();
 const productController = require('../../controllers/productController');
 
-// Define routes for user operations
-router.post('/category', productController.createCategory);  // Create a user
-
+// Define routes for category operations
+router.post('/category', productController.createCategory);  // Create a category
+router.get('/category', productController.getAllCategories);
 module.exports = router;
